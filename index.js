65361f59d3d9126d046eac9d81c5dcbd227a1fea
dawood <dawood.altai111@gmail.com>
2023-04-16 17:34:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+console.log(upperCaseCustomerName())
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = "dawood";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "ahmed";
+}
+
